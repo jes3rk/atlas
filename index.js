c3bc13c78825e91d0660f50d457d30b53ac9687a
jes3rk <joseph.schefer@gmail.com>
2020-01-04 11:12:56 -0500

    building dynamically for github too

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const port = 3000;
 const app = express()
 
-app.get('/', (req, res) => res.send("Hello World"));
+app.get('/', (req, res) => res.sendfile('index.html'))
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
