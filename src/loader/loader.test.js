ff806064d06ec4bd04e427c019e13639b654f15e
jes3rk <joseph.schefer@gmail.com>
2020-04-04 20:28:50 -0400

    initial server set up

--- src/loader/loader.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return value of -1 if value is not present', function() {
-            assert.equal([1,2,3].indexOf(4), -1);
-        })
-    })
-})
