652b033478aa28b69bb3bf8c21459c0db8c51e14
jes3rk <joseph.schefer@gmail.com>
2020-06-27 14:56:37 -0400

    created model

--- project/atlas/models.py
+++ project/atlas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+    house_number = models.CharField(max_length=128)
+    street = models.CharField(max_length=128)
+    city = models.CharField(max_length=64)
+    state = models.CharField(max_length=2)
+    zipcode = models.CharField(max_length=5)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
